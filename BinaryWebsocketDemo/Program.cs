6d4362c649a27f3eb49ca432437b7971dfa6fbb4
Morteza Tavanarad <tavanarad@gmail.com>
2016-01-28 00:24:13 +0330

    init

---
+++ BinaryWebsocketDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author: Morteza
+ * Date: 1/27/2016
+ * Time: 5:10 PM
+ * Copyright: Binary Ltd
+ * 
+ */
+ 
+ 
+using System;
+
+namespace BinaryWebsocketDemo
+{
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+		WebSocketWrapper ws = WebSocketWrapper.Create("wss://ws.binaryws.com/websockets/v3");
+		ws.Connect();
+		ws.OnMessage(OnMessage);
+		ws.OnDisconnect(OnDisconnect);
+		ws.OnConnect(OnConnect);
+		
+		Console.ReadKey(true);
+    }
+    
+    public static void OnMessage(string e, WebSocketWrapper sender){
+    	Console.WriteLine ("Binary says: " + e.ToString());
+    }
+    
+    public static void OnDisconnect(WebSocketWrapper sender){
+    	Console.WriteLine("WebSocket is closed: ");
+    }
+    
+    public static void OnConnect(WebSocketWrapper sender){
+    	sender.SendMessage("{\"ticks\": \"R_100\"}");
+    }
+  }
+}
